cd42adfadbc011232e4e2e69f410a0a0368240fd
OswaldoJos <oswaldojosmanyam2@gmail.com>
2016-08-23 06:38:38 +0200

    Empaquetado de los utilitarios
    Para tener una mejor organizacion de imagenes y archivos
    Tambien se creo una clase constantes para mayor orden.

---
+++ util/ConstantesyFunciones.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.io.File;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author Mercedes
+ */
+public final class ConstantesyFunciones {
+    public static final Image BOTON_MENU_NO_SELECCIONADO = new Image(new File("src/util/imagenes/botonesPrincipal.png").toURI().toString());
+    public static final Image BOTON_MENU_SELECCIONADO = new Image(new File("src/util/imagenes/botonesPrincipal2.png").toURI().toString());
+    public static final Image FONDO_PRINCIPAL = new Image(new File("src/util/imagenes/portada.jpg").toURI().toString());
+    public static final Image FONDO_MAR = new Image(new File("src/util/imagenes/fondoMarino.jpg").toURI().toString());
+    public static final Image BUZO = new Image(new File("src/util/imagenes/buceando.gif").toURI().toString());
+    public static final Image PIRANIA = new Image(new File("src/util/imagenes/pirania").toURI().toString());
+}
