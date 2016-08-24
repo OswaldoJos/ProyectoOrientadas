a37a06c744dc57b5b77ea472f1e2af98f43f578f
edsalloor <edsalloor@hotmail.com>
2016-08-24 14:34:44 +0200

    Add files via upload

--- src/util/ConstantesyFunciones.java
+++ src/util/ConstantesyFunciones.java
@@ -14,9 +14,16 @@ import javafx.scene.image.Image;
  */
 public final class ConstantesyFunciones {
     public static final Image BOTON_MENU_NO_SELECCIONADO = new Image(new File("src/util/imagenes/botonesPrincipal.png").toURI().toString());
+    public static final Image BOTON = new Image(new File("src/util/imagenes/etimadera.png").toURI().toString());
     public static final Image BOTON_MENU_SELECCIONADO = new Image(new File("src/util/imagenes/botonesPrincipal2.png").toURI().toString());
     public static final Image FONDO_PRINCIPAL = new Image(new File("src/util/imagenes/portada.jpg").toURI().toString());
     public static final Image FONDO_MAR = new Image(new File("src/util/imagenes/fondoMarino.jpg").toURI().toString());
     public static final Image BUZO = new Image(new File("src/util/imagenes/buceando.gif").toURI().toString());
     public static final Image PIRANIA = new Image(new File("src/util/imagenes/pirania").toURI().toString());
+    public static final Image UNO = new Image(new File("src/util/imagenes/1.png").toURI().toString());
+    public static final Image DOS = new Image(new File("src/util/imagenes/2.png").toURI().toString());
+    public static final Image TRES = new Image(new File("src/util/imagenes/3.png").toURI().toString());
+    public static final Image CUATRO = new Image(new File("src/util/imagenes/4.png").toURI().toString());
+    public static final Image CINCO = new Image(new File("src/util/imagenes/4.png").toURI().toString());
+    public static final Image CORAZON = new Image(new File("src/util/imagenes/corazon.png").toURI().toString());
 }
